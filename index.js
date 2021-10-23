45be36129d1b81f047ce8ceb5cc0676a49c9dba6
Chris Zichko <chriszichkocoding@gmail.com>
2021-10-23 12:35:42 -0400

    Prompts are finished

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
@@ -7,7 +7,51 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: `What is your project's title?`
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: `Please describe this project:`
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: `How to install this project?`
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: `What is the usage of this project?`
+        },
+        {
+            type: 'checkbox',
+            name: 'licenses',
+            message: `Choose one of the following licenses:`, 
+            choices: ['choice 1', 'choice 2', 'choice 3', 'choice 4', 'choice 5']
+        },
+        {
+            type: 'input',
+            name: 'contributions', 
+            message: `What is the policy for contributuins?`
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: `What tests are allowed and have been done?`
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: `What is your email?`
+        }
+    ]);
+}
 
 // Function call to initialize app
 init();
